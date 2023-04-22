4b7fd18989c59156c245a892ee0e60a1dd85baf8
Ichihai1415 <projects31415.gif@gmail.com>
2023-04-22 10:48:53 +0900

    v1.0.1 ブックマーク・設定を仮追加。一部処理改修。

---
+++ SimpleBrowser/Bookmark.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SimpleBrowser
+{
+    public partial class Bookmark : Form
+    {
+        public Bookmark()
+        {
+            InitializeComponent();
+        }
+
+        private void Bookmark_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (File.Exists("Bookmark.csv"))
+                {
+                    string[] bookmarks = File.ReadAllText("Bookmark.csv").Split('\n');
+                    foreach (string bookmark in bookmarks)
+                    {
+                        string[] bookmark_ = bookmark.Split(',');
+                        Bookmarks.Rows.Add(bookmark_[0], bookmark_[1]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ブックマークの取得に失敗しました。" + ex.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string csv = "\n";
+                foreach (DataGridViewRow data in Bookmarks.Rows)
+                    csv += $"\n{data.Cells[0].Value},{data.Cells[1].Value}";
+                csv = csv.Replace("\n\n", "");
+                File.WriteAllText("Bookmark.csv", csv);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ブックマークの保存に失敗しました。" + ex.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void AutoSave_Tick(object sender, EventArgs e)
+        {
+            Save_Click(sender, e);
+        }
+
+        private void Bookmark_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Save_Click(sender, e);
+        }
+    }
+}
--- SimpleBrowser/Main.cs
+++ SimpleBrowser/Main.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Web.WebView2.Core;
 using SimpleBrowser.Properties;
 using System;
+using System.Configuration;
 using System.Drawing;
+using System.IO;
 using System.Windows.Forms;
 
 namespace SimpleBrowser
@@ -11,16 +13,15 @@ namespace SimpleBrowser
         public Form1()
         {
             InitializeComponent();
+            MainBrowser.Source = new Uri("https://Ichihai1415.github.io/programs/simple-browser/default.html?ver=1.0.1");
         }
 
         private void Form1_Resize(object sender, EventArgs e)
         {
-            URLtext.Size = new Size(ClientSize.Width - 140, 19);
-            MainBrowser.Size = ClientSize - new Size(MainBrowser.Location);
-            LoadPage.Left = ClientSize.Width - 130;
-            Back.Left = ClientSize.Width - 64;
-            Forward.Left = ClientSize.Width - 44;
-            Reload.Left = ClientSize.Width - 24;
+            URLtext.Size = new Size(ClientSize.Width - 160, 19);
+            ButtonsBack.Location = new Point(ClientSize.Width - 150, 5);
+            MainBack.Size = ClientSize - new Size(MainBack.Location);
+            MainBrowser.Size = MainBack.Size;
         }
 
         private void View_Click(object sender, EventArgs e)
@@ -48,12 +49,21 @@ namespace SimpleBrowser
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            Configuration Config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.PerUserRoamingAndLocal);
+            if (File.Exists("UserSetting.xml"))//AppDataに保存
+            {
+                if (!Directory.Exists(Config.FilePath.Replace("\\user.config", "")))//実質更新時
+                    Directory.CreateDirectory(Config.FilePath.Replace("\\user.config", ""));
+                File.Copy("UserSetting.xml", Config.FilePath, true);
+            }
             Settings.Default.Reload();
-                Location = Settings.Default.MainWindowLocation;
+            Location = Settings.Default.MainWindowLocation;
             if (Settings.Default.MainWindowMax)
                 WindowState = FormWindowState.Maximized;
             else
                 Size = Settings.Default.MainWindowSize;
+            if (Settings.Default.WebView2Font != null)
+                MainBack.Font = Settings.Default.WebView2Font;
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
@@ -61,7 +71,7 @@ namespace SimpleBrowser
             Settings.Default.MainWindowSize = Size;
             Settings.Default.MainWindowLocation = Location;
             if (WindowState == FormWindowState.Maximized)
-            Settings.Default.MainWindowMax = true;
+                Settings.Default.MainWindowMax = true;
             else
                 Settings.Default.MainWindowMax = false;
             Settings.Default.Save();
@@ -70,14 +80,6 @@ namespace SimpleBrowser
         private void MainBrowser_SourceChanged(object sender, CoreWebView2SourceChangedEventArgs e)
         {
             URLtext.Text = MainBrowser.CoreWebView2.Source;
-            if (MainBrowser.CoreWebView2.CanGoBack)
-                Back.Enabled = true;
-            else
-                Back.Enabled = false;
-            if (MainBrowser.CoreWebView2.CanGoForward)
-                Forward.Enabled = true;
-            else
-                Forward.Enabled = false;
         }
 
         private void Back_Click(object sender, EventArgs e)
@@ -93,6 +95,36 @@ namespace SimpleBrowser
         private void Reload_Click(object sender, EventArgs e)
         {
             MainBrowser.CoreWebView2.Reload();
+        }
+
+        private void OpenBookmark_Click(object sender, EventArgs e)
+        {
+            Bookmark bookmark = new Bookmark();
+            bookmark.Show();
+        }
+
+        private void OpenSetting_Click(object sender, EventArgs e)
+        {
+            SettingForm settingForm = new SettingForm();
+            settingForm.Show();
+        }
+
+        private void MainBrowser_NavigationStarting(object sender, CoreWebView2NavigationStartingEventArgs e)
+        {
+            LoadPage.Enabled = false;
+            Back.Enabled = false;
+            Forward.Enabled = false;
+            Reload.Enabled = false;
+        }
+
+        private void MainBrowser_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            LoadPage.Enabled = true;
+            Reload.Enabled = true;
+            if (MainBrowser.CoreWebView2.CanGoBack)
+                Back.Enabled = true;
+            if (MainBrowser.CoreWebView2.CanGoForward)
+                Forward.Enabled = true;
         }
     }
 }
